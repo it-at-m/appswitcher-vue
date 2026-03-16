a8a24c33f24dd5bde115a63e55e43defdcf6a314
langehm <38280183+langehm@users.noreply.github.com>
2026-03-16 11:32:41 +0100

    :truck: moved eslint config and adjusted to refarch

---
+++ eslint.config.js
@@ -0,0 +1,28 @@
+import jsEslintConfig from "@eslint/js";
+import vuePrettierEslintConfigSkipFormatting from "@vue/eslint-config-prettier/skip-formatting";
+import {
+  defineConfigWithVueTs,
+  vueTsConfigs,
+} from "@vue/eslint-config-typescript";
+import { ESLint } from "eslint";
+import vueEslintConfig from "eslint-plugin-vue";
+import vuetify from "eslint-plugin-vuetify";
+import { globalIgnores } from "eslint/config";
+
+export default defineConfigWithVueTs(
+  ESLint.defaultConfig,
+  jsEslintConfig.configs.recommended,
+  vueEslintConfig.configs["flat/recommended"],
+  vuetify.configs["flat/base"],
+  vueTsConfigs.recommended,
+  vuePrettierEslintConfigSkipFormatting,
+  globalIgnores([
+    "dist",
+    "dist-demo",
+    "target",
+    "coverage",
+    "node_modules",
+    "env.d.ts",
+    "src/vite-env.d.ts",
+  ])
+);
