abcc18ac8bd9f05b38eb1445ddeb3625624df934
devtobi <28538704+devtobi@users.noreply.github.com>
2026-09-07 08:58:35 +0200

    :wrench: Fix deprecated config

--- vitest.config.ts
+++ vitest.config.ts
@@ -13,8 +13,10 @@ export default defineConfig({
   test: {
     globals: true,
     environment: "jsdom",
-    deps: {
-      inline: ["vuetify"],
+    server: {
+      deps: {
+        inline: ["vuetify"],
+      },
     },
     setupFiles: "tests/setup.js",
   },
